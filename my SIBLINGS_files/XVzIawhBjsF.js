125d9e7ba44e97c14d787261a334545a562a3cf1
Chikadibia5 <oyohemmanuel@rocketmail.com>
2023-03-17 19:13:56 +0100

    Update API

---
+++ my SIBLINGS_files/XVzIawhBjsF.js
@@ -0,0 +1,9 @@
+;/*FB_PKG_DELIM*/
+
+__d("CometMarketplaceHashtagFeedPageHeaderQuery_facebookRelayOperation",[],(function(a,b,c,d,e,f){e.exports="7113522348719136"}),null);
+__d("CometMarketplaceHashtagFollowButtonContainerQuery_facebookRelayOperation",[],(function(a,b,c,d,e,f){e.exports="5177088882328860"}),null);
+__d("CometMarketplaceLeftRailNavigationContainerQuery_facebookRelayOperation",[],(function(a,b,c,d,e,f){e.exports="7850349191672214"}),null);
+__d("CometMarketplaceNotificationsListContainerQuery_facebookRelayOperation",[],(function(a,b,c,d,e,f){e.exports="8523192774420375"}),null);
+__d("CometMarketplaceRootQuery_facebookRelayOperation",[],(function(a,b,c,d,e,f){e.exports="5051403454957854"}),null);
+__d("MarketplaceBannerContainerQuery_facebookRelayOperation",[],(function(a,b,c,d,e,f){e.exports="5960489097344879"}),null);
+__d("MarketplaceCometBrowseFeedLightContainerQuery_facebookRelayOperation",[],(function(a,b,c,d,e,f){e.exports="5815640788517402"}),null);
